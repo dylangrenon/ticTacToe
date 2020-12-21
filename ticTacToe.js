2cc39bbfff96b5b436104ab06848997bb4c3c2f2
dylangrenon <54150298+dylangrenon@users.noreply.github.com>
2020-12-20 21:45:31 -0500

    Add files via upload

---
+++ ticTacToe.js
@@ -0,0 +1,99 @@
+var cellOne = document.querySelector("#one");
+var cellTwo = document.querySelector("#two");
+var cellThree = document.querySelector("#three");
+var cellFour = document.querySelector("#four");
+var cellFive = document.querySelector("#five");
+var cellSix = document.querySelector("#six");
+var cellSeven = document.querySelector("#seven");
+var cellEight = document.querySelector("#eight");
+var cellNine = document.querySelector("#nine");
+
+// cell one
+cellOne.addEventListener("click",function(){
+  cellOne.textContent = "X";
+});
+
+cellOne.addEventListener("dblclick",function(){
+  cellOne.textContent = "O";
+});
+
+// cell two
+cellTwo.addEventListener("click",function(){
+  cellTwo.textContent = "X";
+});
+
+cellTwo.addEventListener("dblclick",function(){
+  cellTwo.textContent = "O";
+});
+
+// cell three
+cellThree.addEventListener("click",function(){
+  cellThree.textContent = "X";
+});
+
+cellThree.addEventListener("dblclick",function(){
+  cellThree.textContent = "O";
+});
+
+// cell four
+cellFour.addEventListener("click",function(){
+  cellFour.textContent = "X";
+});
+
+cellFour.addEventListener("dblclick",function(){
+  cellFour.textContent = "O";
+});
+
+// cell five
+cellFive.addEventListener("click",function(){
+  cellFive.textContent = "X";
+});
+
+cellFive.addEventListener("dblclick",function(){
+  cellFive.textContent = "O";
+});
+
+// cell six
+cellSix.addEventListener("click",function(){
+  cellSix.textContent = "X";
+});
+
+cellSix.addEventListener("dblclick",function(){
+  cellSix.textContent = "O";
+});
+
+// cell seven
+cellSeven.addEventListener("click",function(){
+  cellSeven.textContent = "X";
+});
+
+cellSeven.addEventListener("dblclick",function(){
+  cellSeven.textContent = "O";
+});
+
+// cell three
+cellThree.addEventListener("click",function(){
+  cellThree.textContent = "X";
+});
+
+cellThree.addEventListener("dblclick",function(){
+  cellThree.textContent = "O";
+});
+
+// cell eight
+cellEight.addEventListener("click",function(){
+  cellEight.textContent = "X";
+});
+
+cellEight.addEventListener("dblclick",function(){
+  cellEight.textContent = "O";
+});
+
+// cell nine
+cellNine.addEventListener("click",function(){
+  cellNine.textContent = "X";
+});
+
+cellNine.addEventListener("dblclick",function(){
+  cellNine.textContent = "O";
+});
